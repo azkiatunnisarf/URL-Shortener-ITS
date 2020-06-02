224d14a6a0a4a52d701fd30b12a56864a3a9b178
Azkiatunnisa Fajriyati <azisarf9@gmail.com>
2020-06-02 13:13:04 +0800

    benerin typo

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -10,7 +10,7 @@
                         <div class="col-md-12"> 
                             <div class="slider-content_wrap"> 
                                 <h1 style="color:white;">ITS Shortlink</h1> 
-                                <h5 style="color:white;">Shortlink byyyyy DPTSI</h5>
+                                <h5 style="color:white;">Shortlink by DPTSI</h5>
                             </div> 
                         </div> 
                     </div> 
