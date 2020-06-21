c1a8aa4386fcb04e6f88f4c2d2711b33f04a6d3c
Azkiatunnisa Fajriyati <azisarf9@gmail.com>
2020-06-21 22:42:01 +0800

    7

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -9,7 +9,7 @@
                     <div class="row"> 
                         <div class="col-md-12"> 
                             <div class="slider-content_wrap"> 
-                                <h1 style="color:white;">ITS-Shortener gogo</h1> 
+                                <h1 style="color:white;">ITS-Shortener gogooo</h1> 
                                 <h5 style="color:white;">Link Shortener by DPTSI Bismillah!</h5>
                             </div> 
                         </div> 
