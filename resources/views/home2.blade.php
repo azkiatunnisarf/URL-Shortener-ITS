3ca5eb24a2e9ca29432c9f51f8a2ffc09dd05008
azkiatunnisarf <azisarf9@gmail.com>
2020-07-02 12:11:27 +0800

    29

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -9,7 +9,7 @@
                     <div class="row"> 
                         <div class="col-md-12"> 
                             <div class="slider-content_wrap"> 
-                                <h1 style="color:white;">ITS-liiiiiiink-Shortener</h1> 
+                                <h1 style="color:white;">ITS-Liiiiiiink-Shortener</h1> 
                                 <h5 style="color:white;">Link Shortener by DPTSI ITS</h5>
                             </div> 
                         </div> 
